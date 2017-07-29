eb5318dd2b23216fef2cad274c74feeb55f897e2
Yutaro HIGASHI <yutaro.higashi@notteco.jp>
2017-07-29 17:36:16 +0900

    ページ遷移に対応

--- lib/sns-data-extractor.rb
+++ lib/sns-data-extractor.rb
@@ -8,22 +8,30 @@ class SNSDataExtractor < Sinatra::Application
   get '/' do
     @oauth = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], "#{request.base_url}/callback")
     @oauth.get_app_access_token
+
+    if !params[:post_since].nil? || !params[:page].nil?
+      #TODO: 権限が無い場合に再度ログインを促す必要あり
+      @graph = Koala::Facebook::API.new(session[:access_token], ENV['FB_APP_SECRET'])
+      @permissions = @graph.get_connections("me", "permissions")
+      @results = params[:page] ? @graph.get_page(JSON.parse(params[:page])) : @graph.get_connections("me", "posts", {since: params[:post_since], until: params[:post_until]})
+    end
+
     erb :index
   end
 
   get '/login' do
-    session['oauth'] = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], "#{request.base_url}/callback")
-    redirect session['oauth'].url_for_oauth_code()
+    session[:oauth] = Koala::Facebook::OAuth.new(ENV['FB_APP_ID'], ENV['FB_APP_SECRET'], "#{request.base_url}/callback")
+    redirect session[:oauth].url_for_oauth_code(:permissions => "user_posts,user_photos")
   end
 
   get '/logout' do
-    session['oauth'] = nil
-    session['access_token'] = nil
+    session[:oauth] = nil
+    session[:access_token] = nil
     redirect '/'
   end
 
   get '/callback' do
-    session['access_token'] = session['oauth'].get_access_token(params[:code])
+    session[:access_token] = session['oauth'].get_access_token(params[:code])
     redirect '/'
   end
 end
