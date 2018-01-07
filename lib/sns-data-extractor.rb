ac612d812381f17149cf10f84fd94a6c5bae0a36
Yutaro HIGASHI <yutaro.higashi@notteco.jp>
2018-01-07 22:41:16 +0900

    同期的に指定した期間の位置情報付き画像一覧の取得機能を作成

--- lib/sns-data-extractor.rb
+++ lib/sns-data-extractor.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'bundler'
 Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 Dotenv.load
@@ -6,105 +8,52 @@ class SNSDataExtractor < Sinatra::Application
   use Rack::Session::Cookie, secret: ENV['SESSION_SECRET']
   register Sinatra::Flash
   FB_FETCH_LIMIT = 1000
-  FB_PERMISSIONS = "user_posts,user_photos"
-  FB_FETCH_FIELDS = "attachments,message,created_time,place"
-
-  connection = Fog::Storage.new({
-    provider:              'AWS',
-    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
-    aws_secret_access_key: ENV['AWS_ACCESS_SECRET'],
-    region: "ap-northeast-1"
-  })
+  FB_PERMISSIONS = 'user_posts,user_photos'
+  FB_FETCH_FIELDS = 'attachments,message,created_time,place'
 
   before do
-     pass if ([nil]+%w[login logout callback]).include? request.path_info.split('/')[1]
-     if session[:access_token].nil?
-       flash[:alert] = "You need login first."
-       redirect '/'
-     end
-   end
+    pass if ([nil] + %w[login logout callback]).include? request.path_info.split('/')[1]
+    if session[:access_token].nil?
+      flash[:alert] = 'You need login first.'
+      redirect '/'
+    end
+  end
 
   get '/' do
     erb :index
   end
 
   get '/list' do
+    connection = getS3Connection
     directory = connection.directories.get(ENV['AWS_S3_BUCKET'], prefix: session[:user_id])
     files = directory.nil? ? [] : directory.files
 
-    @raw_files = files.select{|f| f.key.match?(/#{session[:user_id]}\/raw_.*$/) }.map{|f| f}
-    @map_files = files.select{|f| f.key.match?(/#{session[:user_id]}\/map_.*$/) }.map{|f| f}
+    @raw_files = files.select { |f| f.key.match?(/#{session[:user_id]}\/raw_.*$/) }.map { |f| f }
+    @map_files = files.select { |f| f.key.match?(/#{session[:user_id]}\/map_.*$/) }.map { |f| f }
 
     erb :list, layout: false
   end
 
   get '/fetch' do
-    EM::defer do
-      if !params[:query].nil? || !params[:page].nil?
-        @graph = getGraphAPIObject
+    EM.defer do
+      facebook_post = fetchPost(
+        since: params[:query][:since],
+        til: params[:query][:until]
+      )
+      storeToS3(
+        body: JSON.pretty_generate(facebook_post),
+        key: "#{session[:user_id]}/raw_#{params[:query][:since]}_#{params[:query][:until]}_#{SecureRandom.hex(16)}.json"
+      )
 
-        @graph_result = params[:page] ? @graph.get_page(JSON.parse(params[:page])) :
-                                   @graph.get_connections("me", "posts",
-                                      params[:query].merge({limit: FB_FETCH_LIMIT, fields: FB_FETCH_FIELDS})
-                                    )
-        @results = @graph_result.to_a
-      end
-
-      while !(next_results = @graph_result.next_page).nil? do
-        @results += next_results.to_a
-        @graph_result = next_results
-      end
-      # NGだったら再試行を3回ぐらいやって、駄目なら取れたところまでで返す。
-
-      pretty_json = JSON.pretty_generate(@results)
-
-      directory = connection.directories.get(ENV['AWS_S3_BUCKET'])
-
-      file = directory.files.new({
-        :key    => "#{session[:user_id]}/raw_#{params[:query][:since]}_#{params[:query][:until]}_#{SecureRandom.hex(16)}.json",
-        :body   => pretty_json,
-        :public => true
-      })
-      file.save
-      flash[:notice] = "Raw Data successfully generated."
-
-      pposts = @results.select{|j| !j["place"].nil?}
-
-      map_datas = pposts.map do |ppost|
-          {
-            created_time: ppost["created_time"],
-            message: ppost["message"],
-            name: ppost["place"]["name"],
-            latitude: ppost["place"]["location"]["latitude"],
-            longitude: ppost["place"]["location"]["longitude"],
-            images: ppost["attachments"]["data"].map do |sa|
-              next if sa["type"] != "photo" && sa["type"] != "album"
-
-              result = []
-              result << sa["media"]["image"]["src"] if !sa["media"].nil? && !sa["media"]["image"].nil?
-              result += sa["subattachments"]["data"].map do |s|
-                "ERROR: media または media/imageがありません。" if s["media"].nil? || s["media"]["image"].nil?
-                s["media"]["image"]["src"] if !s["media"].nil? && !s["media"]["image"].nil?
-              end if !sa["subattachments"].nil?
-              result
-            end.compact.flatten
-          }
-      end
-
-      directory = connection.directories.get(ENV['AWS_S3_BUCKET'])
-
-      file = directory.files.new({
-        :key    => "#{session[:user_id]}/map_#{params[:query][:since]}_#{params[:query][:until]}_#{SecureRandom.hex(16)}.json",
-        :body   => JSON.pretty_generate(map_datas),
-        :public => true
-      })
-      file.save
-
-      flash[:notice] = "Map Data successfully generated."
-
-
+      map_json = makeMapJSON(facebook_post)
+      storeToS3(
+        body: JSON.pretty_generate(map_json),
+        key: "#{session[:user_id]}/map_#{params[:query][:since]}_#{params[:query][:until]}_#{SecureRandom.hex(16)}.json"
+      )
+      flash[:notice] = 'Successfully generated.'
     end
-    flash[:notice] = "Successfully generating. It may take awhile."
+
+    flash[:notice] = 'Successfully generating. It may take awhile.'
     redirect '/'
   end
 
@@ -114,7 +63,7 @@ class SNSDataExtractor < Sinatra::Application
   end
 
   get '/logout' do
-    flash[:notice] = "You successfully logged out."
+    flash[:notice] = 'You successfully logged out.'
 
     session[:oauth] = nil
     session[:access_token] = nil
@@ -137,19 +86,96 @@ class SNSDataExtractor < Sinatra::Application
       batch_api.get_object('me/permissions')
     end
 
-    session[:user_id] = me["id"]
+    session[:user_id] = me['id']
 
-    lack_permissions = FB_PERMISSIONS.split(",") - permissions.select{|i| i[:status] == "granted"}.map{|i| i[:permission]}
+    lack_permissions = FB_PERMISSIONS.split(',') - permissions.select { |i| i[:status] == 'granted' }.map { |i| i[:permission] }
     flash[:alert] = "Lack of permission(s). It may causes problem while fetching data. : #{lack_permissions}" if lack_permissions.count == 0
 
-    flash[:notice] = "You successfully logged in."
+    flash[:notice] = 'You successfully logged in.'
     redirect '/'
   end
 
+  get '/mapdata' do
+    if params[:query].nil? || params[:query][:since].nil? || params[:query][:until].nil?
+      return "no parameter error : #{params}"
+    end
+
+    facebook_post = fetchPost(since: params[:query][:since], til: params[:query][:until])
+
+    content_type :json
+    JSON.pretty_generate(makeMapJSON(facebook_post))
+  end
 
   def getGraphAPIObject
     return nil if session[:access_token].nil?
     Koala::Facebook::API.new(session[:access_token], ENV['FB_APP_SECRET'])
   end
 
+  def getS3Connection
+    Fog::Storage.new(
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_ACCESS_SECRET'],
+      region: 'ap-northeast-1'
+    )
+  end
+
+  def fetchPost(since:, til:, query: nil)
+    graph = getGraphAPIObject
+
+    graph_result = graph.get_connections('me', 'posts',
+                                         limit: FB_FETCH_LIMIT,
+                                         fields: FB_FETCH_FIELDS,
+                                         since: since,
+                                         until: til)
+
+    results = graph_result.to_a
+
+    unless (next_results = graph_result.next_page).nil?
+      results += next_results.to_a
+      graph_result = next_results
+    end
+
+    results
+  end
+
+  def makeMapJSON(facebook_post)
+    facebook_post.reject{ |j| j['place'].nil? }.map do |ppost|
+      {
+        created_time: ppost['created_time'],
+        message: ppost['message'],
+        name: ppost['place']['name'],
+        latitude: ppost['place']['location']['latitude'],
+        longitude: ppost['place']['location']['longitude'],
+        images: ppost['attachments']['data'].map do |sa|
+          next if sa['type'] != 'photo' && sa['type'] != 'album'
+
+          result = []
+          result << sa['media']['image']['src'] if !sa['media'].nil? && !sa['media']['image'].nil?
+          unless sa['subattachments'].nil?
+            result += sa['subattachments']['data'].map do |s|
+              'ERROR: media または media/imageがありません。' if s['media'].nil? || s['media']['image'].nil?
+              s['media']['image']['src'] if !s['media'].nil? && !s['media']['image'].nil?
+            end
+          end
+          result
+        end.compact.flatten
+      }
+    end
+
+    # if storeToS3(key: "#{session[:user_id]}/map_#{since}_#{til}_#{SecureRandom.hex(16)}.json", body: JSON.pretty_generate(map_datas))
+    #   flash[:notice] = 'Map Data successfully generated.'
+    # end
+  end
+
+  def storeToS3(key:, body:, isPublic: true)
+    connection = getS3Connection
+    directory = connection.directories.get(ENV['AWS_S3_BUCKET'])
+    file = directory.files.new(
+      key: key,
+      body: body,
+      public: isPublic
+    )
+    file.save
+  end
 end
